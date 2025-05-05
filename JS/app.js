6355b6c034efb7d674e21c7b5b47acfe44cd9c6c
IPIA-14 <ipiajuanjo7@gmail.com>
2025-05-05 12:27:42 -0500

    Ejemplos javascript

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+//numero1 = parseInt(numero1) es igualque la linea 1 pero esta uno es mas corta
+
+let numero1 = parseInt(prompt("Ingrese un Número: "))
+let numero2 = parseInt(prompt("Ingrese un Número: "))
+//document.writeln("Suma: " + (numero1 + numero2) + "<br>")
+//document.writeln(`Suma: ${numero1 - numero2}`)
+
+let Suma=numero1+numero2
+let resta=numero1-numero2
+let mult=numero1*numero2
+let div=numero1/numero2
+
+//document.writeln("Suma = " + (Suma) + "<br>")
+//document.writeln("Resta = " + (resta) + "<br>")
+//document.writeln("Multiplicacion = " + (mult) + "<br>")
+//document.writeln("Division = " + (div) + "<br>")
+
